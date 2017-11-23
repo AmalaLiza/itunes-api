c059f81ad8106a277cd48dc815e0c2f60cda4cbc
AmalaLiza <amala.james@razorthink.com>
2017-11-23 20:41:47 +0530

    background Color change for selected artist

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React, { Component } from 'react';
-import ListItem from '../List/ListItem/ListItem';
 import List from '../List/List';
+import ListItem from '../List/ListItem/ListItem';
 import styles from './Artists.css';
 
 /**
@@ -10,12 +10,27 @@ import styles from './Artists.css';
  **/
 
 class Artists extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      selected: '',
+    };
+  }
+
+  onClick(artist) {
+    this.props.onClick(artist);
+    this.setState({ selected: artist.get('name') });
+  }
+
   render() {
-    const { artists, onClick } = this.props;
+    const { artists } = this.props;
+    const { selected } = this.state;
     return (
       <List className={styles.list}>
-        {artists.map((artist) => <ListItem className={styles.listItem}
-                                     onClick={() => onClick(artist)}>
+        {artists.map((artist) => <ListItem
+          className={classNames(styles.listItem, { [styles.selected]: selected === artist.get('name') })}
+          onClick={() => this.onClick(artist)}>
           <div className={styles.artist}>
           </div>
           <div className={classNames(styles.listItemSinger, styles['text-ellipsis'])}>{artist.get('name')}</div>
